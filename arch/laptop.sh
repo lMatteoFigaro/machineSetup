2b2b7728a315dcb71dca3b4a296a7716ff3c1f8e
Lucas MATTEO <lmatteo@figarocms.fr>
2025-10-11 15:54:09 +0200

    Remove service

--- arch/laptop.sh
+++ arch/laptop.sh
@@ -6,9 +6,6 @@ sudo pacman -S --noconfirm --needed power-profiles-daemon
 if ls /sys/class/power_supply/BAT* &>/dev/null; then
   # This computer runs on a battery
   powerprofilesctl set balanced || true
-
-  # Enable battery monitoring timer for low battery notifications
-  systemctl --user enable --now omarchy-battery-monitor.timer
 else
   # This computer runs on power outlet
   powerprofilesctl set performance || true
