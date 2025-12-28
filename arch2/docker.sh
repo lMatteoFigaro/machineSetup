e70202b3b1f33ba80bca84fb6c563acfc70bedc6
Lucas MATTEO <lmatteo@figarocms.fr>
2025-12-28 18:57:23 +0000

    Add docker install

---
+++ arch2/docker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo pacman -Syu --noconfirm
+# Install Docker
+sudo pacman -S docker docker-compose --noconfirm
+
+sudo systemctl enable --now docker.service
+
+sudo usermod -aG docker $USER
+
+echo "Installation complete."
+echo "IMPORTANT: Please log out and log back in (or restart) for the group changes to take effect."
