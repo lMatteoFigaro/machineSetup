0c9f004a4ff6be490456885b8e919d5d17d1005e
Lucas MATTEO <lmatteo@figarocms.fr>
2025-12-29 10:30:04 +0100

    Add lazygit and lazydocker

--- arch2/install.sh
+++ arch2/install.sh
@@ -1,5 +1,5 @@
 sudo pacman -S --noconfirm git vim zsh tmux stow fzf curl btop ripgrep pkg-config jq 
-sudo pacman -S --noconfirm  hyprland hyprpaper waybar hypridle hyprlock hyprpolkitagent qt5-wayland qt6-wayland grim slurp wofi nautilus iwd impala ghostty neovim openssh wl-clipboard base-devel wiremix
+sudo pacman -S --noconfirm  hyprland hyprpaper waybar hypridle hyprlock hyprpolkitagent qt5-wayland qt6-wayland grim slurp wofi nautilus iwd impala ghostty neovim openssh wl-clipboard base-devel wiremix lazygit lazydocker
 sudo pacman -S --noconfirm  xdg-desktop-portal-hyprland
 
 sudo pacman -S --noconfirm go lua nm-connection-editor
