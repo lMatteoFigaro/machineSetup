1ed4babfe043fc8217a17b1031ab1704211da55f
Lucas MATTEO <lmatteo@figarocms.fr>
2026-01-01 13:42:39 +0100

    add mako to install

--- arch2/install.sh
+++ arch2/install.sh
@@ -1,4 +1,4 @@
-sudo pacman -S --noconfirm git vim zsh tmux stow fzf curl btop ripgrep pkg-config jq 
+sudo pacman -S --noconfirm git vim zsh tmux stow fzf curl btop ripgrep pkg-config jq mako
 sudo pacman -S --noconfirm  hyprland hyprpaper waybar hypridle hyprlock hyprpolkitagent qt5-wayland qt6-wayland grim slurp wofi nautilus iwd impala ghostty neovim openssh wl-clipboard base-devel wiremix lazygit lazydocker unzip zip fd
 sudo pacman -S --noconfirm  xdg-desktop-portal-hyprland
 
